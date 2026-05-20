5517f57d242a952abd1e3b7c6dc068cf147c779c
tenggys <levgood21@yandex.ru>
2026-05-20 18:30:26 +0500

    build project

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/brand-vue3-scss/',
+  //base: '/brand-vue3-scss/',
   plugins: [
     vue(),
     vueDevTools(),
